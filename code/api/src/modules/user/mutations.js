a97e4e4d3c87f352b543c4d68c88b2876770ad53
Jordan Sewell <jrsewell400@gmail.com>
2020-07-10 11:14:39 -0600

    Add update function definition

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove } from './resolvers'
+import { create, remove, update } from './resolvers'
 
 // Create
 export const userSignup = {
@@ -25,6 +25,23 @@ export const userSignup = {
     }
   },
   resolve: create
+}
+
+// Update
+export const userUpdate = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+
+    stylePreferences: {
+      name: 'stylePreferences',
+      type: GraphQLString
+    },
+  },
+  resolve: update
 }
 
 // Remove
