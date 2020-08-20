030ce1458ca5c07862fa214a82bf1982c7eff197
Kyle Wong <kylewong2510@gmail.com>
2020-08-20 17:24:08 -0600

    Annotate H4 component

--- code/web/src/ui/typography/H4.js
+++ code/web/src/ui/typography/H4.js
@@ -7,9 +7,13 @@ import { primary, secondary } from '../common/fonts'
 
 // Component
 const H4 = (props) => {
+  // component receives props and destructures for use in render
   const { children, font, ...others } = props
 
   return (
+    // passes all other props into the h3 tag.  what can the html element do with these props?
+    // renders children between H3 tags when component is rendered
+    // inline styling determined by font prop
     <h4 {...others}>
       {children}
 
