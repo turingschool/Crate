290ebb5077de43432a04481632a75bc2e815b33f
Antonio Jackson <antoniojackson@gmail.com>
2020-08-20 14:21:02 -0600

    Adding annotations to schema index file

--- code/api/src/setup/schema/index.js
+++ code/api/src/setup/schema/index.js
@@ -1,11 +1,11 @@
 // Imports
 import { GraphQLSchema } from 'graphql'
 
-// App Imports
+// App Imports - This code imports the query and mutation specifics from their respective files
 import query from './queries'
 import mutation from './mutations'
 
-// Schema
+// Schema - This code creates the schema with the imported query and mutation specifics
 const schema = new GraphQLSchema({
   query,
   mutation
