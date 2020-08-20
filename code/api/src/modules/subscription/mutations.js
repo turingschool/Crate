2c81e94f18561c86049992cc171a25322e90a3af
Antonio Jackson <antoniojackson@gmail.com>
2020-08-20 15:28:06 -0600

    Adding annotations to subscription mutations

--- code/api/src/modules/subscription/mutations.js
+++ code/api/src/modules/subscription/mutations.js
@@ -1,14 +1,14 @@
 // Imports
 import { GraphQLInt } from 'graphql'
 
-// App Imports
+// App Imports - This code block imports the subscription types and create/remove resolvers
 import SubscriptionType from './types'
 import { create, remove } from './resolvers'
 
 // Subscription create
 export const subscriptionCreate = {
   type: SubscriptionType,
-  args: {
+  args: { // These are the allowed args for subscription create actions
     crateId: {
       name: 'crateId',
       type: GraphQLInt
@@ -20,7 +20,7 @@ export const subscriptionCreate = {
 // Subscription remove
 export const subscriptionRemove = {
   type: SubscriptionType,
-  args: {
+  args: { // These are the allowed args for the subscription remove action
     id: {
       name: 'id',
       type: GraphQLInt
