4482f735da2473cb779fd6d2dd3458eac6b8e4b6
Antonio Jackson <antoniojackson@gmail.com>
2020-08-20 14:20:30 -0600

    Adding annotations to schema queries file

--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -1,13 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
-// App Imports
+// App Imports - This code imports the allowed query specifics for each object type from their respective files
 import * as user from '../../modules/user/query'
 import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 
-// Query
+// Query - This code defines the allowed query specifics for each object type based on the imported info
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'API Queries [Read]',
