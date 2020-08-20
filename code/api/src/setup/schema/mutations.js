69fdec7ac57c314461f455a70d5359449e5da5fb
Antonio Jackson <antoniojackson@gmail.com>
2020-08-20 14:20:06 -0600

    Adding annotations to schema mutations

--- code/api/src/setup/schema/mutations.js
+++ code/api/src/setup/schema/mutations.js
@@ -1,13 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
-// App Imports
+// App Imports - This code imports the allowed mutation specifics for each object type from their respective folders
 import * as user from '../../modules/user/mutations'
 import * as product from '../../modules/product/mutations'
 import * as crate from '../../modules/crate/mutations'
 import * as subscription from '../../modules/subscription/mutations'
 
-// Mutation
+// Mutation - This code defines the allowed mutations based on the object type
 const mutation = new GraphQLObjectType({
   name: 'mutations',
   description: 'API Mutations [Create, Update, Delete]',
