d3d467494a3ce021cf6eee01657f8599a0b776e3
Antonio Jackson <antoniojackson@gmail.com>
2020-08-20 15:27:33 -0600

    Adding annotations to subscription types file

--- code/api/src/modules/subscription/types.js
+++ code/api/src/modules/subscription/types.js
@@ -2,16 +2,16 @@
 import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
-import { UserType } from '../user/types'
-import CrateType from '../crate/types'
-
+import { UserType } from '../user/types' // This code imports the user type
+import CrateType from '../crate/types' // This code imports the crate type
+// We will probably need to import the user type in the style types.js file
 // Subscription type
 const SubscriptionType = new GraphQLObjectType({
   name: 'subscription',
   description: 'Subscription Type',
 
   fields: () => ({
-    id: { type: GraphQLInt },
+    id: { type: GraphQLInt }, // It appears this is where the requirements for the field types are set (ie: the id must be a GraphQLInt, the user a UserType, etc.)
     user: { type: UserType },
     crate: { type: CrateType },
     createdAt: { type: GraphQLString },
