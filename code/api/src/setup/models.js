82f748e5bd6af0e32771ced6d2b9c753a1814c75
Antonio Jackson <antoniojackson@gmail.com>
2020-08-23 12:41:09 -0600

    Annotating the models setup to show new imports may be needed

--- code/api/src/setup/models.js
+++ code/api/src/setup/models.js
@@ -9,7 +9,7 @@ const models = {
   Product: databaseConnection.import('../modules/product/model'),
   Crate: databaseConnection.import('../modules/crate/model'),
   Subscription: databaseConnection.import('../modules/subscription/model') // This appears to be making a database connection and then importing the Subscription model
-} // We will probably need to add a line to import the new Style model
+} // We will probably need to add a line to import the new Style and StylePrefs models
 
 // This code block appears to be providing the instructions for creating an association/relationship between two different models based on the model passed in and whether or not an associate relationship is defined in the model
 Object.keys(models).forEach(modelName => {
