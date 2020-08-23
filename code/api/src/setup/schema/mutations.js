8fd6f7b97c25a8cd2e091b6bc9be81e045b0fa6d
Antonio Jackson <antoniojackson@gmail.com>
2020-08-23 12:42:01 -0600

    Annotating the schema queries and mutations

--- code/api/src/setup/schema/mutations.js
+++ code/api/src/setup/schema/mutations.js
@@ -1,13 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
-// App Imports - This code imports the allowed mutation specifics for each object type from their respective folders
+// App Imports - This code imports the allowed mutation specifics for each object type from their respective folders - we would import style and stylePrefs mutation paths
 import * as user from '../../modules/user/mutations'
 import * as product from '../../modules/product/mutations'
 import * as crate from '../../modules/crate/mutations'
 import * as subscription from '../../modules/subscription/mutations'
 
-// Mutation - This code defines the allowed mutations based on the object type
+// Mutation - This code defines the allowed mutations based on the object type - If we create Styles and stylePrefs, we would just need to add those to the fields
 const mutation = new GraphQLObjectType({
   name: 'mutations',
   description: 'API Mutations [Create, Update, Delete]',
--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -1,13 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
-// App Imports - This code imports the allowed query specifics for each object type from their respective files
+// App Imports - This code imports the allowed query specifics for each object type from their respective files - we would need to import the style & stylePrefs queries
 import * as user from '../../modules/user/query'
 import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 
-// Query - This code defines the allowed query specifics for each object type based on the imported info
+// Query - This code defines the allowed query specifics for each object type based on the imported info - we would need to add style & stylePrefs here to the fields
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'API Queries [Read]',
