7263792b20cb43a0f59f7cf37e7ab3b71f259250
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-08-24 15:01:14 -0600

    Routes setup

--- code/web/src/setup/routes/survey.js
+++ code/web/src/setup/routes/survey.js
@@ -1,8 +1,15 @@
 import SurveyPage from '../../modules/survey/SurveyPage'
+import SurveyResults from '../../modules/survey/SurveyResults'
 
 export default {
 	survey: {
     path: '/style-survey',
+    component: SurveyPage,
+    auth: true
+	},
+
+	surveyResults: {
+		path: '/style-preference',
     component: SurveyPage,
     auth: true
 	}
