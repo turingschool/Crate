63c79ed016cdf192b621819026ed2afe983ea671
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-24 19:33:51 -0600

    Add user query tests

--- code/api/src/modules/user/user.test.js
+++ code/api/src/modules/user/user.test.js
@@ -19,9 +19,29 @@ describe('user queries',  () => {
     )
   })
 
+  it('returns all users', async () =>{
+    const response = await request(server)
+      .get('/')
+      .send({ query: '{ users { name email }}'})
+      // we can send a mutation here instead of a query
+      .expect(200)
+
+    expect(response.body.data.users.length).toEqual(2)
+    expect(response.body.data.users[0].email).toBe('admin@crate.com')
+    expect(response.body.data.users[0].name).toBe('The Admin')
+  })
+
+  it('returns user by id', async () => {
+    const response = await request(server)
+    .get('/')
+    .send({ query: '{user(id: 2) {email name} }'})
+    .expect(200)
+
+  expect(response.body.data.user.name).toBe('The User')
+  expect(response.body.data.user.email).toBe('user@crate.com')
+  })
+
   it('is true', () => {
     expect(true).toBe(true)
   })
-
-  
 })
