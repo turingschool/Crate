afc862aef7e29799f1bb6aaba93b1941548a3f97
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-25 14:59:02 -0600

    Added Helmet to header to component

--- code/web/src/modules/user/StyleSurvey.js
+++ code/web/src/modules/user/StyleSurvey.js
@@ -1,30 +1,48 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-import { APP_URL } from '../../setup/config/env'
-import userRoutes from '../../setup/routes/user'
+// UI Imports
+import { Grid, GridCell } from "../../ui/grid";
+import { H3, H4 } from "../../ui/typography";
+import Button from "../../ui/button";
+import Icon from "../../ui/icon";
+import { textLevel1 } from "../../ui/common/shadows";
+import { white, grey, grey2, grey3 } from "../../ui/common/colors";
+
+import { APP_URL } from "../../setup/config/env";
+import userRoutes from "../../setup/routes/user";
+
 
 class StyleSurvey extends Component {
   constructor(props) {
-    super(props)
-
+    super(props);
     this.state = {
-      style : null
-    }
+      style: null,
+    };
+    console.log(this.props);
   }
 
+  render() {
+    return (
+      <section>
+        <Helmet>
+          <title>My Style Preferences</title>
+        </Helmet>
+        <Grid style={{ backgroundColor: grey }}>
+      <GridCell style={{ padding: "2em", textAlign: "center" }}>
+        <H3 font="secondary">My Style Preferences</H3>
 
-render() {
-  return (
-    <h1>Look at this style!</h1>
-
-  )
-
+        <p style={{ marginTop: "1em", color: grey2 }}>
+          Please complete the survey below.
+        </p>
+      </GridCell>
+    </Grid>
+      </section>
+    );
+  }
 }
 
-}
-
-StyleSurvey.propTypes = {
-}
+StyleSurvey.propTypes = {};
 
 export default StyleSurvey;
