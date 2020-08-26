8813cc36df080f747be58967338fb7249d2daa26
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-25 19:26:26 -0600

    Test authorized user

--- code/api/src/modules/user/queries.test.js
+++ code/api/src/modules/user/queries.test.js
@@ -2,6 +2,9 @@ import request from 'supertest'
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
+import models from '../../setup/models'
+import bcrypt from 'bcrypt';
+import db from '../../setup/database';
 
 describe('user queries',  () => {
   let server;
@@ -18,28 +21,66 @@ describe('user queries',  () => {
     )
   })
 
-  it('returns all users', async () =>{
+  beforeEach(async() => {
+   const dracoMalfoy = {
+     id: 1,
+     name: "Draco Malfoy",
+     email: "malfoy@hogwarts.com",
+     password: bcrypt.hashSync('slytherin', 10),
+     role: "USER",
+     createdAt: new Date(),
+     updatedAt: new Date(),
+   };
+
+   const rubeusHagrid = {
+     id: 2,
+     name: "Rubeus Hagrid",
+     email: "grounds@hogwarts.com",
+     password: bcrypt.hashSync('buckbeak', 10),
+     role: "USER",
+     createdAt: new Date(),
+     updatedAt: new Date(),
+   };
+
+   await models.User.create(rubeusHagrid);
+   await models.User.create(dracoMalfoy);
+ })
+
+ afterEach(async() => {
+   await models.User.destroy({ where: {}})
+ })
+
+ afterAll(() => {
+   db.close()
+ })
+
+  it('it returns all users', async () => {
     const response = await request(server)
       .get('/')
       .send({ query: '{ users { name email }}'})
       .expect(200)
 
     expect(response.body.data.users.length).toEqual(2)
-    expect(response.body.data.users[0].email).toBe('admin@crate.com')
-    expect(response.body.data.users[0].name).toBe('The Admin')
+    expect(response.body.data.users[0].email).toBe('grounds@hogwarts.com')
+    expect(response.body.data.users[0].name).toBe('Rubeus Hagrid')
   })
 
-  it('returns user by id', async () => {
+  it('it returns user by id', async () => {
     const response = await request(server)
     .get('/')
-    .send({ query: '{user(id: 2) {email name} }'})
+    .send({ query: '{user(id: 1) {email name} }'})
     .expect(200)
 
-  expect(response.body.data.user.name).toBe('The User')
-  expect(response.body.data.user.email).toBe('user@crate.com')
+  expect(response.body.data.user.name).toBe('Draco Malfoy')
+  expect(response.body.data.user.email).toBe('malfoy@hogwarts.com')
   })
 
-  it('is true', () => {
-    expect(true).toBe(true)
+  it('it can authorize a user', async () => {
+  const response = await request(server)
+    .get('/')
+    .send({ query: '{ userLogin(email: "malfoy@hogwarts.com", password: "slytherin") { user { id name role email } } }'})
+    .expect(200)
+
+    expect(response.body.data.userLogin.user.name).toEqual("Draco Malfoy")
   })
 })
