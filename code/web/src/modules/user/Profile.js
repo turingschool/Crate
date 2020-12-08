a198d9d7b95d7b19208eb021d7f3c5d197009a66
Stacy Potten <stacylynnaz@gmail.com>
2020-12-08 15:36:14 -0700

    Add additional user data placeholders

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -5,13 +5,17 @@ import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 
+
 // UI Imports
+import ImageTile from '../../ui/image/Tile'
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import Button from '../../ui/button'
 import { grey, grey2 } from '../../ui/common/colors'
+import { level1 } from '../../ui/common/shadows'
 
 // App Imports
+import { APP_URL } from '../../setup/config/env'
 import userRoutes from '../../setup/routes/user'
 import { logout } from './api/actions'
 
@@ -32,15 +36,31 @@ const Profile = (props) => (
 
     <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+        {/*<Tile style={{}} image={icon}></Tile>*/}
+        <ImageTile width={300} height={530} shadow={level1} image={`${ APP_URL }/images/stock/men/1.jpg`} />
+      </GridCell>
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
         <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
-
+        <p style={{ marginBottom: '0.5em' }}>Personal description goes here</p>
+        {/*Description text should be dynamic from user details ie props.user.details.description */}
+      </GridCell>
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
         <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>
-
-        <Link to={userRoutes.subscriptions.path}>
+        <p style={{ color: grey2, marginBottom: '2em' }}>123 West 5th St City, State 80654</p>
+        {/*Address text should be dynamic from user details ie props.user.details.shipping */}
+      </GridCell>
+      {/*<Link to={userRoutes.subscriptions.path}>
           <Button theme="primary">Subscriptions</Button>
+      </Link>
+<Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>*/}
+    </Grid>
+    <Grid>
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+        <Link to={userRoutes.subscriptions.path}>
+              <Button theme="primary">Subscriptions</Button>
         </Link>
-
         <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
+        <Button theme="primary" style={{ marginLeft: '1em' }}>Edit Info</Button>
       </GridCell>
     </Grid>
   </div>
