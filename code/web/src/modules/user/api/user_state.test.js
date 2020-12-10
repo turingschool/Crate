b94fd1059381e46828ca2d651a919f5ac8d6ba49
Orlando Murcio <ato.murcio@gmail.com>
2020-12-10 14:10:24 -0700

    Make test more readable

--- code/web/src/modules/user/api/user_state.test.js
+++ code/web/src/modules/user/api/user_state.test.js
@@ -2,11 +2,10 @@ import  * as actions from './actions'
 import  userReducer from './state'
 import { isEmpty } from '../../../setup/helpers'
 import '@testing-library/jest-dom';
-import thunk from 'redux-thunk';
-
+import { waitFor } from '@testing-library/react';
 describe('userActions', () => {
 
-  let fakeState, reducer, fakeUser, fakeLoginState;
+  let fakeInitialState, reducer, fakeUser, fakeLoginState;
 
     beforeEach(() =>{
 
@@ -18,7 +17,7 @@ describe('userActions', () => {
 
       reducer = userReducer;
 
-      fakeState = {
+      fakeInitialState = {
         error: null,
         isLoading: false,
         isAuthenticated: false,
@@ -39,7 +38,7 @@ describe('userActions', () => {
   // it('should work', () => {
   //   //test ASIGN_STYLE
     
-  //   expect(reducer(fakeState, {
+  //   expect(reducer(fakeInitialState, {
 
   //   }))
   //   .toEqual(true)
@@ -48,38 +47,51 @@ describe('userActions', () => {
   // })
 
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(fakeState)
+    const result = reducer(undefined, {})
+    expect(result).toEqual(fakeInitialState)
   });
   
   
   it('should be able to make a LOGIN_REQUEST', () => {
-    expect(reducer(fakeState, {
-      type: actions.LOGIN_REQUEST,
-      isLoading: true
-    }
-    )).toEqual(
-      {
-        "details": null,
-        "error": null,
-        "isAuthenticated": false,
-        "isLoading": true,
-        "isSurveyCompleted": false
-      }
-      )
-    })
-    
-    it.skip('should be able to SET_USER when logged in', () => {
-      expect(reducer(fakeState, {
-        type: actions.SET_USER,
-        isAuthenticated: !isEmpty(fakeUser),
-        details: fakeUser
+    const action = {type: actions.LOGIN_REQUEST,isLoading: true}
 
+    const result = reducer(fakeInitialState, action)
+
+    const newState = {
+      "details": null,
+      "error": null,
+      "isAuthenticated": false,
+      "isLoading": true,
+      "isSurveyCompleted": false
+    }
+
+    expect(result).toEqual(newState)
+  })
+    
+    it('should return updated state when SET_USER is called', async() => {
+
+      const action = {
+        type: actions.SET_USER, 
+        isAuthenticated: true,
+        details: fakeUser,
+        user: fakeUser
       }
-      )).toEqual({fakeLoginState})
+
+      const result = reducer(fakeInitialState, action)
+
+      const newState = {
+        error: null,
+        isLoading: false,
+        isAuthenticated: true,
+        details: fakeUser,
+      };
+      
+      await waitFor (() => expect(result).toEqual(newState))
+
     });
 
     it('should be able to make a LOGIN_RESPONSE', ()=> {
-      expect(reducer(fakeState, {
+      expect(reducer(fakeInitialState, {
         type: actions.LOGIN_RESPONSE,
         error: 'fake error',
         isLoading: false
@@ -96,7 +108,7 @@ describe('userActions', () => {
     })
 
     it('should be able to LOG_OUT', ()=> {
-      expect(reducer(fakeState, {
+      expect(reducer(fakeInitialState, {
         type: actions.LOGOUT,
         error: null,
         isLoading: false,
