43c1f0db9cf48bbce4ea04b27e8c025f86ec959c
Travis McKinstry <65680281+TravisGM92@users.noreply.github.com>
2020-12-09 17:13:15 -0700

    Update model.js
    Removed some annotations

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -5,7 +5,6 @@ module.exports = function(sequelize, DataTypes) {
   // sequlize.define starts a SQL command to create a table
   let User = sequelize.define('users', {
     name: {
-      // the 'name' is an attribute, the 'type:' defines what kind of attribute it is, which is followed by it's type, .STRING. The difference between .STRING and .TEXT is .TEXT allows for a larger character limit
       type: DataTypes.STRING
     },
     email: {
@@ -18,7 +17,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT
     }
   })
-  // the User.associate below is where the relationship between Subscription and User is defined. User 'has many' Subscriptions
   User.associate = function(models) {
     User.hasMany(models.Subscription)
   }
