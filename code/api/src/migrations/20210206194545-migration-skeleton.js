38e9272316f0682bbeefcf5697eeebd847cd5d9f
Kevin Cuadros <kev040798@gmail.com>
2021-02-06 14:55:43 -0500

    Created the shipping address table and deleted past migrations.
    Co-authored-by: Carson Jardine <carsonjardine@gmail.com>
    Co-authored-by: Grant Dempsey <cryptodemps@gmail.com>

---
+++ code/api/src/migrations/20210206194545-migration-skeleton.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('shippingAddress', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      street1: {
+        type: Sequelize.STRING
+      },
+      street2: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      zip: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('shippingAddress');
+  }
+};
