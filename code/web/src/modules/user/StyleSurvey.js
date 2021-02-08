e272b1439d69b5e6a2d2515c86de919c3a782cd7
Jordon Smith <jordon00smith@gmail.com>
2021-02-08 13:19:28 -0700

    Add route fro style-preferences

---
+++ code/web/src/modules/user/StyleSurvey.js
@@ -0,0 +1,16 @@
+// Imports
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
+
+// UI Imports
+import { Grid, GridCell } from '../../ui/grid'
+import { H3, H4 } from '../../ui/typography'
+import Button from '../../ui/button'
+import { grey, grey2 } from '../../ui/common/colors'
+
+// App Imports
+import userRoutes from '../../setup/routes/user'
+
