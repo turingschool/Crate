13b0791133be74a7550e0d881424e4bc89ce5a20
Jordon Smith <jordon00smith@gmail.com>
2021-02-08 13:35:44 -0700

    Setup StyleSurvey component to appear on route

--- code/web/src/modules/user/StyleSurvey.js
+++ code/web/src/modules/user/StyleSurvey.js
@@ -1,9 +1,9 @@
 // Imports
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
@@ -14,3 +14,31 @@ import { grey, grey2 } from '../../ui/common/colors'
 // App Imports
 import userRoutes from '../../setup/routes/user'
 
+class StyleSurvey extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      bohemian: 0,
+      sporty: 0,
+      preppy: 0,
+      punk: 0,
+      questionNum: 1
+    }
+  }
+
+  render() {
+    return(
+      <h1>Style Survey</h1>
+    )
+  }
+
+
+}
+
+function styleSurveyState(state) {
+  return {
+    user: state.user
+  }
+}
+export default connect(styleSurveyState)(withRouter(StyleSurvey))
