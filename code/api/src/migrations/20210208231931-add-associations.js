b1879eff3b00f0bac310dd9ce270e1154ef512f8
Carson Jardine <carsonjardine@gmail.com>
2021-02-08 19:09:41 -0700

    Fix migration for fk constraint
    Co-Authored-By: Kevin David Cuadros <63522369+kevxo@users.noreply.github.com>
    Co-Authored-By: Grant Dempsey <CryptoDemps@gmail.com>

--- code/api/src/migrations/20210208231931-add-associations.js
+++ code/api/src/migrations/20210208231931-add-associations.js
@@ -6,7 +6,7 @@ module.exports = {
       'products', // name of Source model
       'crateId', // name of the key we're adding
       {
-        type: Sequelize.UUID,
+        type: Sequelize.INTEGER,
         references: {
           model: 'crates', // name of Target model
           key: 'id', // key in Target model that we're referencing
@@ -17,17 +17,10 @@ module.exports = {
     );
   },
 
-  // down: (queryInterface, Sequelize) => {
-  //   return queryInterface.removeColumn(
-  //     'Products', // name of Source model
-  //     'crateId' // key we want to remove
-  //   );
-  // }
-
-  down(queryInterface, Sequelize) {
-    // logic for reverting the changes
-    return Promise.all([
-      queryInterface.removeColumn('products', 'crateId'),
-    ]);
-  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'Products', // name of Source model
+      'crateId' // key we want to remove
+    );
+  }
 };
