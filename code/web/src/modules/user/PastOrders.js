2d90c457332a8f6018b144d1e5363fb10efe5d3c
JP Carey <jpcarey4@gmail.com>
2021-02-09 18:42:19 -0700

    style out columns for crates

--- code/web/src/modules/user/PastOrders.js
+++ code/web/src/modules/user/PastOrders.js
@@ -1,10 +1,104 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { Grid, GridCell } from '../../ui/grid'
+import { H2, H6 } from '../../ui/typography'
+import { grey, grey4, secondary } from '../../ui/common/colors'
 
 class PastOrders extends PureComponent {
+
+    componentDidMount() {
+
+    }
+
     render() {
         return (
-            <h1>Hello!</h1>
+            <GridCell style={{ backgroundColor: secondary, height: "100%"}}>
+                <H2>My Past Orders</H2>
+                <Grid style={{ backgroundColor: grey, margin: "3em", padding: '.5em' }}>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                </Grid>
+                <Grid style={{ backgroundColor: grey, margin: "3em", padding: '.5em' }}>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                    <GridCell style={{ textAlign: "center"}}>
+                        <img 
+                            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTmxYkmH9BRcMKj8QFHWz8AU92943C0djkzylgdY8N3ElCVq2S6tqJoX0dTw5F7M9KCAmKMGA2W84oBwLpbnG47OBAFIqOso4OkX4T7lAs&usqp=CAc"
+                            style={{ width: "125px"}}
+                        />
+                        <H6>Jeans</H6>
+                        <p>You Kept These!</p>
+                    </GridCell>
+                </Grid>
+            </GridCell>
         )
     }
 }
