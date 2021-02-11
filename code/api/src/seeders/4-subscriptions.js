e9c577def9d1b2ee03f152b7967ed495e23a483e
Carson Jardine <carsonjardine@gmail.com>
2021-02-10 18:26:10 -0700

    Update seeders with the correct information

--- code/api/src/seeders/4-subscriptions.js
+++ code/api/src/seeders/4-subscriptions.js
@@ -5,21 +5,35 @@ module.exports = {
     return queryInterface.bulkInsert('subscriptions', [
       {
         userId: 2,
+<<<<<<< HEAD
         crateId: 2,
+=======
+        crateId: 1,
+>>>>>>> c76c03d... Update seeders with the correct information
         createdAt: new Date(),
         updatedAt: new Date(),
         deliveryDate: new Date()
       },
       {
+<<<<<<< HEAD
         userId: 1,
         crateId: 2,
+=======
+        userId: 2,
+        crateId: 3,
+>>>>>>> c76c03d... Update seeders with the correct information
         createdAt: new Date(),
         updatedAt: new Date(),
         deliveryDate: new Date()
       },
       {
+<<<<<<< HEAD
         userId: 2,
         crateId: 1,
+=======
+        userId: 1,
+        crateId: 2,
+>>>>>>> c76c03d... Update seeders with the correct information
         createdAt: new Date(),
         updatedAt: new Date(),
         deliveryDate: new Date()
